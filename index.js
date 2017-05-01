fa37e7aeed008936eaf583758c2e6f3dc54ba9bd
Vinny Martinez <vinny@vinnymartinez.com>
2017-04-30 22:04:58 -0400

    passed again

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 //Code React element here
+// const meInReact = React.createClass({
+//   render: function() {
+//     return (
+//       React.createElement('li', {},
+//         React.createElement('h2', {}, 'hi')
+//       )
+//     )
+//   },
+// });
+//
+var meInReact =
+  React.createElement('div', {className: 'me'},
+    React.createElement('h1', {}, 'An Awesome Person'),
+    React.createElement('p', {}, 'Who is learning React'),
+    React.createElement('ul', {className: 'me__interests'},
+      React.createElement('li', {}, 'JavaScript'),
+      React.createElement('li', {}, 'React'),
+      React.createElement('li', {}, 'Movies'),
+      React.createElement('li', {}, 'Ice cream')
+    )
+  );
+
+ReactDOM.render(meInReact, document.getElementById('main'));
